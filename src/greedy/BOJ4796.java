27568a59d7a3008c2ad402fed63983ff58124ff7
sksmsy <jyh94502@naver.com>
2023-02-03 21:12:34 +0900

    230203

--- src/greedy/BOJ4796.java
+++ src/greedy/BOJ4796.java
@@ -5,12 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/*
+ * 
+ * 
+ */
+
 public class BOJ4796 {
 
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int l, p, v;
 		StringBuilder sb = new StringBuilder();
 		
