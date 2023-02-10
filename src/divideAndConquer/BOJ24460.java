85abd84412767aeb4104e75a4554161849d09964
sksmsy <jyh94502@naver.com>
2023-02-10 23:32:50 +0900

    230210

--- src/divideAndConquer/BOJ24460.java
+++ src/divideAndConquer/BOJ24460.java
@@ -1,5 +1,11 @@
 package divideAndConquer;
 
-public class BOJ24460 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class BOJ24460 {
+	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
 }
