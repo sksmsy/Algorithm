3bc10026a3d73fdc2729bc7b4ef6f44fd708154a
sksmsy <jyh94502@naver.com>
2023-02-28 23:53:09 +0900

    230228

--- src/bitMasking/BOJ11723.java
+++ src/bitMasking/BOJ11723.java
@@ -3,60 +3,65 @@ package bitMasking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
-
+/*
+ * 
+ * 
+ * 집
+ */
 public class BOJ11723 {
+	  static int N;
 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int m = Integer.parseInt(br.readLine());
-		
-		
-		StringTokenizer st;
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			
-			String pr = st.nextToken();
-			if(pr.equals("add") || pr.equals("check") || pr.equals("toggle")) {
-				
-				int x = Integer.parseInt(st.nextToken());
-				
-				if(pr.equals("add")) {
-					arr.add(x);
-				}else if(pr.equals("check")) {
-					if(arr.contains(x)) {
-						System.out.println(1);
-					}else {
-						System.out.println(0);
-					}
-				}else if(pr.equals("remove")) {
-					if(arr.contains(x)) {
-						String tmp = ""+x;
-						
-						arr.remove(tmp);
-					}
-				}else if(pr.equals("toggle")) {
-					if(arr.contains(x)) {
-						String tmp = ""+x;
-						arr.remove(tmp);
-					}else {
-						arr.add(x);
-					}
-				}
-			}else if(pr.equals("all")){
-				arr.clear();
-				for (int j = 1; j <= 20; j++) {
-					arr.add(j);
-				}
-			}else {
-				arr.clear();
-			}
-			
-			
-			
-			
-		}
+	    public static void main(String[] args) throws NumberFormatException, IOException {
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        StringTokenizer st;
+	        StringBuilder sb = new StringBuilder();
+	        N = Integer.parseInt(br.readLine());
+	        int k = 0;
+	        for (int i = 0; i < 20; i++) {
+	            k += 1 << i;
+	        }
+	        int s = 1 << 20;
+
+	        while (N-- > 0) {
+	            st = new StringTokenizer(br.readLine());
+
+	            String str = st.nextToken();
+	            int num = 0;
+	            switch (str) {
+	            case "all":
+	                s = k;
+	                break;
+	            case "empty":
+	                s &= 0;
+	                break;
+	            case "add":
+	                num = Integer.parseInt(st.nextToken()) - 1;
+	                s |= 1 << num;
+	                break;
+	            case "remove":
+	                num = Integer.parseInt(st.nextToken()) - 1;
+	                s &= ~(1 << num);
+	                break;
+	            case "check":
+	                num = Integer.parseInt(st.nextToken()) - 1;
+
+	                if (((s >> num) & 1) == 1) {
+	                    sb.append(1).append("\n");
+	                } else {
+	                    sb.append(0).append("\n");
+	                }
+
+	                break;
+	            case "toggle":
+	                num = Integer.parseInt(st.nextToken()) - 1;
+	                s ^= (1 << num);
+	                break;
+	            default:
+	                break;
+	            }
+
+	        }
+	        System.out.println(sb);
 	}
 }
