270edd5916f84f31278d5f414a31897b5c0359a3
sksmsy <jyh94502@naver.com>
2023-03-01 22:45:49 +0900

    230301

--- src/bitMasking/BOJ14936.java
+++ src/bitMasking/BOJ14936.java
@@ -1,13 +1,14 @@
 package bitMasking;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ14936 {
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
